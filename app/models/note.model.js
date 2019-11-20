97f7aed770e74ae5fc43f5eef63bcf1dcc37d4a0
sergeyshestak <shestak.sergey.a@gmail.com>
2019-11-20 19:07:35 +0300

    CRUD api template

---
+++ app/models/note.model.js
@@ -0,0 +1,10 @@
+const mongose = require('mongoose');
+
+const NoteSchema = mongose.Schema({
+    title: String,
+    content: String
+}, {
+    timestamps: true
+});
+
+module.exports = mongose.model('Note', NoteSchema);
